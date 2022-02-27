2f10a14135d43ae3a7ea6858fbba0e8aa21b370d
PedagogiaDHBrasil <pedagogiabrasil@digitalhouse.com>
2022-02-27 18:20:40 -0300

    desafios

---
+++ 01-desafios/desafios-Blobs.js
@@ -0,0 +1,17 @@
+// var n = parseInt(gets());
+var n = 333;
+var c = parseFloat()
+var max = 1000
+var min = 1
+
+while (n >= min  && n <= max ) {
+//   var c = parseFloat(gets());
+  var c = 5.99
+var dias = 0;
+  while ( c > min && c <= max ) {
+    c = c / 2.0;
+    dias++;
+  }
+  n--;
+  console.log(`${dias}  dias`);
+}
